0da4b8fef8009f5c1f7c7117446937ca0ebd8fd5
eduardorittner <eduardorittnerc@gmail.com>
2024-12-02 09:21:44 -0300

    download mathjax with https

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -16,7 +16,7 @@ fn format_header(title: &str, root: &str) -> String {
     }}); \
     </script> \
     <script type=\"text/javascript\" \
-    src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"> \
+    src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"> \
     </script> \
     </head> \
     ",
