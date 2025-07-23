d82131e127baeafd0325c5741f27f05772ec3dfa
eduardorittner <eduardorittnerc@gmail.com>
2025-07-23 13:27:12 -0300

    Remove async! Simplify! Delete code!
    Removed all async since I don't think it actually improves performance
    that much (it's basically instant either way) and it actually slows down
    compilation by quite a lot. Also removed external url validation, which
    I'll probably add again later. Any link errors are logged but do not
    impede the build process

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -1,5 +1,4 @@
-use async_std::task;
-use pandalib::builder::{ExternalLinkValidator, Site};
+use pandalib::builder::Site;
 use std::{path::PathBuf, sync::Arc};
 
 fn main() {
@@ -8,15 +7,5 @@ fn main() {
     let root = PathBuf::from("../src");
     let to = PathBuf::from("../output");
 
-    if args.get(1).is_none_or(|s| s != "validate") {
-        if let Err(e) = task::block_on(Site::build(to, root)) {
-            println!("ERROR: {:?}", e);
-            std::process::exit(1);
-        };
-    } else {
-        if let Err(e) = task::block_on(Site::build_with_url_validator(to, root)) {
-            println!("ERROR: {:?}", e);
-            std::process::exit(1);
-        }
-    }
+    Site::build(to, root);
 }
